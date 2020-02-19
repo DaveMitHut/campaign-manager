cc53645ce3f7f09f113e925ed8a6c31ea90b3220
Moritz Esser <moritz_esser@hotmail.de>
2020-02-19 22:58:42 +0100

    Views update, readme, remove: db.sqlite3

--- campaignmanager/campaignmanager/urls.py
+++ campaignmanager/campaignmanager/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('user/', include('usermanagement.urls')),
+    path('usermanagement/', include('usermanagement.urls')),
     path('admin/', admin.site.urls),
 ]
--- campaignmanager/usermanagement/views.py
+++ campaignmanager/usermanagement/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.template import loader
 from django.contrib.auth import authenticate, login
+
+def isLoggedIn(request):
+    if request.user.is_authenticated:
+        return redirect('index')
 
 # Create your views here.
 def index_view(request):
     return HttpResponse("Hello World! You are looking at the user management index.")
 
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponse("Successfully logged in.")
+    isLoggedIn(request)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Successfully logged in.")
     
+        else:
+            return HttpResponse("Something went wrong.")
     else:
-        return HttpResponse("Something went wrong.")
+        form = AuthenticationForm
+        return render(request,'usermanagement/index.html',{'form':form})
 
 
 def register_view(request):
+    isLoggedIn(request)
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid:
@@ -34,3 +45,4 @@ def register_view(request):
         form = UserCreationForm
 
     return render(request, 'usermanagement/index.html', {'form': form})
+

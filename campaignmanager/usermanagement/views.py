c6a2c4e536e393454d1e1cbfa5aa8e7154a30c54
David Huseyin <davemithut@googlemail.com>
2020-02-19 21:57:31 +0100

    Add user registration to usermanagement app

--- campaignmanager/usermanagement/views.py
+++ campaignmanager/usermanagement/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.template import loader
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World! You are looking at the user management index.")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('index')
+
+    else:
+        form = UserCreationForm
+
+    template = loader.get_template('usermanagement/index.html')
+    return HttpResponse(template.render({'form': form}))
